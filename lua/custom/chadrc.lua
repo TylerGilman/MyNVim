de1d0dfd5e821b89a4dfb330f2fb33bb79288450
Tyler Gilman <tygilman@veliolabs.com>
2024-02-13 01:39:04 +0100

    added harpoon

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,9 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = 'onedark' }
+M.ui = { tabufline = {
+  enabled= false
+},
+  theme = 'catppuccin'}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -75,11 +75,11 @@ local options = {
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm {
+    ["<Tab>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-j>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif require("luasnip").expand_or_jumpable() then
