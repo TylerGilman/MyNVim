2cc976b3c6955451bf727b21019acf8c09129d81
Tyler Gilman <tylersgilman@gmail.com>
2024-06-11 23:17:29 -0400

    dumb

--- nvim/lua/custom/chadrc.lua
+++
@@ -1,9 +0,0 @@
----@type ChadrcConfig
-local M = {}
-M.ui = { tabufline = {
-  enabled= false
-},
-  theme = 'falcon'}
-M.plugins = "custom.plugins"
-M.mappings = require "custom.mappings"
-return M
